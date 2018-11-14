a9703f3c561c9b890716f3dd618f682939f7c781
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-14 15:05:55 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,92 +1,31 @@
-const Discord = require('discord.js');
+onst Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = 'R'
-const developers = ['320423357709549568'];
+const developers = ['335484868479811584'];
 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(`~`,'https://www.twitch.tv/Mdax77x')
-  client.user.setStatus("Do Not Disturb")
+  client.user.setGame(`~`,'https://www.twitch.tv/MeeRcY')
+  client.user.setStatus("idle")
 });
-
-
-
-client.on('message', message => {
-if (message.content.startsWith('السلام عليكم')){
-     let ra3d = new Discord.RichEmbed()
-  .setColor("#e9ab26")
-  .setDescription("**وعليكم السلام ورحمة الله وبركاتة**")
-     
-     
-  message.channel.sendEmbed(ra3d);
-    }
-});
-
-
-
-const bannedwords = [
-    "كل زق",
-    "كس",
-    "كس امك",
-    "الله يلعن",
-    "قحبه",
-    "خنيث"
-
-  ];
-
-client.on('message',  message => {
-  if(bannedwords.some(word => message.content.includes(word))) {
-    message.delete()
-    message.reply(" احترم نفسك , يمنع الشتم في خادمنا او سوف تتعرض الي  ميوت ").then(msg => {msg.delete(5000)});;
-  };
-});
-
-
-
-
-client.on('message', message => {
-    let prefix = 'R';
-    
-    if(message.content.startsWith(prefix + 'getid')) {
-        let mentionned = message.mentions.users.first();
-            if(mentionned) {
-                var c = mentionned;
-            } else {
-                var c = message.author;
-            }
-                let embed = new Discord.RichEmbed()
-                    .setColor("RANDOM")
-                    .setTitle(`**[ ${c.id} ]**`);
-
-                    message.channel.sendEmbed(embed);
-
-    }
-});
-
-
-
-
-
-
 
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!developers.includes(message.author.id)) return;
 
-if (message.content.startsWith('Rwc')) {
+if (message.content.startsWith('Rwt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
-  client.user.setStatus("Do Not Disturb")
+  client.user.setStatus("idle")
     message.channel.send(` ** ${argresult} \ ** `)
 } else 
 if (message.content.startsWith('Rls')) {
 client.user.setActivity(argresult , {type:'LISTENING'});
-  client.user.setStatus("Do Not Disturb")
+  client.user.setStatus("idle")
     message.channel.send(` ** ${argresult} \ ** `)
 } else 
 if (message.content.startsWith('Rst')) {
   client.user.setGame(argresult, "https://www.twitch.tv/MeeRcY");
-  client.user.setStatus("Do Not Disturb")
+  client.user.setStatus("idle")
    message.channel.send(` ** ${argresult} \ ** `)
 }
 });
@@ -103,7 +42,4 @@ client.on('message', message => {
   }
           
 });
-
-
-
 client.login(process.env.BOT_TOKEN);
