e50b86e294a4fff89a8ced89ad052b40b9bd7e2a
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-14 14:41:15 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,57 +1,16 @@
-const Discord = require('discord.js');
+onst Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
-
-client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`صيآنة`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
 
-
-
-
-
+const developers = ['335484868479811584'];
 
 client.on('ready', function(){    
-    var ms = 40000 ;    
-    var setGame = [` #be Your Self : `,"Raped"];    
-    var i = -1;    
-    var j = 0;    
-    setInterval(function (){    
-        if( i == -1 ){    
-j = 1;    
-       }    
-        if( i == (setGame.length)-1 ){    
-            j = -1;    
-      }    
-       i = i+j;    
+    if (message.content.startsWith('Rw')){
+            var setGame = [` #be Your Self : `,"Raped"];    
         client.user.setGame(setGame[i],`http://twitch.tv/S-F`);    
-}, ms);    
-    
-});
+          client.user.setStatus("Do Not Disturb")
 
-
+    var setGame = [` #be Your Self : `,"Raped"];    
 
 
 
