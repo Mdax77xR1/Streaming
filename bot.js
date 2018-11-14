2b8d41d38ebe1f9f39024b3c4be426023f74a2bb
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-14 15:20:47 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,45 +1,56 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const developers = ['335484868479811584  '];
 
+  const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(`~`,'https://www.twitch.tv/MeeRcY')
-  client.user.setStatus("DND ")
+client.user.setGame(`صيآنة`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!developers.includes(message.author.id)) return;
 
-if (message.content.startsWith('Rwt')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-  client.user.setStatus("DND "")
-    message.channel.send(` ** ${argresult} \ ** `)
-} else 
-if (message.content.startsWith('Rls')) {
-client.user.setActivity(argresult , {type:'LISTENING'});
-  client.user.setStatus("DND ")
-    message.channel.send(` ** ${argresult} \ ** `)
-} else 
-if (message.content.startsWith('Rst')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/MeeRcY");
-  client.user.setStatus("DND ")
-   message.channel.send(` ** ${argresult} \ ** `)
-}
-});
 
-client.on('message', message => {
-        if (!developers.includes(message.author.id)) return;
-  if (message.content === '0..') {
-  let channel = client.channels.get('511744532992229377');
 
-  channel.join()
-  .then(connection => console.log('Connected'))
-  .catch(console.error);
+
+
+client.on('ready', function(){    
+    var ms = 40000 ;    
+    var setGame = [`** الآحترآم لآيصلح للجميع فآلبعض لآيقتنع حتىء يهآننءء **`,"xR1"];    
+    var i = -1;    
+    var j = 0;    
+    setInterval(function (){    
+        if( i == -1 ){    
+j = 1;    
+       }    
+        if( i == (setGame.length)-1 ){    
+            j = -1;    
+      }    
+       i = i+j;    
+        client.user.setGame(setGame[i],`http://twitch.tv/S-F`);    
+}, ms);    
     
-  }
-          
 });
+
+
 client.login(process.env.BOT_TOKEN);
