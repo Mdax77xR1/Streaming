9b0e1427b9dd9b55c9ef9d7ced3057c9328f552e
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-23 13:27:24 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,4 +53,44 @@ j = 1;
 });
 
 
+
+
+
+
+
+
+
+const adminprefix = "X";
+const devs = ['335484868479811584','335484868479811584'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+      } else     
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+  } else
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+  } else    
+if (message.content.startsWith(adminprefix + 'sett')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Mdax77xR1");//xR1 Server
+}
+});
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
